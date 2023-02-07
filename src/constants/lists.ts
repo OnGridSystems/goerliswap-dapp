09866299497499768e822f38ae1e2c0e8ff6aa93
Tanya <t.kozlova@ongrid.pro>
2023-02-07 16:09:59 +0300

    feat: add custom tokens on local node

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,12 +1,9 @@
 // used to mark unsupported tokens, these are hosted lists of unsupported tokens
 
 const COMPOUND_LIST = 'https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json'
-const UMA_LIST = 'https://umaproject.org/uma.tokenlist.json'
 const AAVE_LIST = 'tokenlist.aave.eth'
 const SYNTHETIX_LIST = 'synths.snx.eth'
 const WRAPPED_LIST = 'wrapped.tokensoft.eth'
-const SET_LIST = 'https://raw.githubusercontent.com/SetProtocol/uniswap-tokenlist/main/set.tokenlist.json'
-const OPYN_LIST = 'https://raw.githubusercontent.com/opynfinance/opyn-tokenlist/master/opyn-v1.tokenlist.json'
 const ROLL_LIST = 'https://app.tryroll.com/tokens.json'
 const COINGECKO_LIST = 'https://tokens.coingecko.com/uniswap/all.json'
 const CMC_ALL_LIST = 'defi.cmc.eth'
@@ -20,7 +17,7 @@ const CUSTOM_TOKEN_LIST =
   'https://raw.githubusercontent.com/OnGridSystems/goerliswap-dapp/development-3.3.6/src/constants/tokenLists/custom.tokenlist.json'
 
 const LOCALNODE_TOKEN_LIST =
-  'https://raw.githubusercontent.com/OnGridSystems/goerliswap-dapp/3-3-6-release/src/constants/tokenLists/localnode.tokenlist.json'
+  'https://raw.githubusercontent.com/OnGridSystems/goerliswap-dapp/local-node/src/constants/tokenLists/localnode.tokenlist.json'
 
 export const UNSUPPORTED_LIST_URLS: string[] = [BA_LIST]
 
@@ -32,10 +29,7 @@ export const DEFAULT_LIST_OF_LISTS: string[] = [
   COMPOUND_LIST,
   AAVE_LIST,
   SYNTHETIX_LIST,
-  UMA_LIST,
   WRAPPED_LIST,
-  SET_LIST,
-  OPYN_LIST,
   ROLL_LIST,
   COINGECKO_LIST,
   CMC_ALL_LIST,
